e055ec0844308e1c4a675a93857947b68538afc9
luke <sharlock@gmail.com>
2018-04-16 20:42:07 +1000

    finish the possibility calculation feature

--- bit_string_posibility.py
+++ bit_string_posibility.py
@@ -2,24 +2,30 @@ from bit_string import BitString
 
 
 def main():
-    long_string_len = 10
-    short_string_len = 3
-    brute_force(long_string_len, short_string_len)
-    # possibility_calculation(long_string_len, short_string_len)
+    long_string_len = 32
+    short_string_len = 5
+    # brute_force(long_string_len, short_string_len)
 
-    result = possibility_calculation(long_string_len, short_string_len)
-    print(result)
+    result_dict = count_calculation(long_string_len, short_string_len)
+    possibility = calculate_possibility(result_dict, long_string_len)
+    print(possibility)
 
+def calculate_possibility(result_dict, long_string_len):
+    possibility = sum([v/(2**long_string_len)
+                      for v in result_dict.values()])/len(result_dict)
+    return possibility
 
-def possibility_calculation(long_string_len, short_string_len):
+def count_calculation(long_string_len, short_string_len):
     long_list = [-1 for x in range(long_string_len)]
-    print(long_list)
-
-    short_list = [1, 1, 0]
-    # for short_list in BitString(short_string_len):
-
-    final_count = iterate_position(long_list, short_list, 0, 0)
-    return final_count
+    # print(long_list)
+    result_dict = {}
+    # short_list = [1, 1, 0]
+    for short_list in BitString(short_string_len):
+        short_string = ''.join(str(x) for x in short_list)
+        final_count = iterate_position(long_list, short_list, 0, 0)
+        result_dict[short_string] = final_count
+        print(str(short_list)+":"+str(final_count))
+    return result_dict
 
 
 def iterate_position(long_list, short_list, start, count):
@@ -31,35 +37,38 @@ def iterate_position(long_list, short_list, start, count):
 
 
 def count_appear(current_list, short_list, start, short_list_count):
-    result_list, position, find = find_the_possible_position(
-        current_list, short_list, start)
+    result_list, find = check_the_possible_position(current_list, short_list, start)
     if find:
         short_list_count += 1
         minus_one_count = sum([1 for e in result_list if e == -1])
         possibliity = (-1)**(short_list_count+1) * (2**minus_one_count)
-        print(result_list)
-        print("###"+str(possibliity) + ":" + str(position)+":"+str(start))
+        # print(result_list)
+        # print("###"+str(possibliity) + ":" + str(start)+":"+str(start))
         possibliity += iterate_position(result_list,
-                                        short_list, position+1, short_list_count)
+                                        short_list, start+1, short_list_count)
         return possibliity
     else:
-        print("@@@"+str(start) + ":" + str(short_list_count))
+        # print("@@@"+str(start) + ":" + str(short_list_count))
         return 0
 
 
-def find_the_possible_position(current_list, short_list, start):
+def check_the_possible_position(current_list, short_list, position):
     new_list = current_list[:]
-    for i in range(start, len(new_list)-len(short_list)+1):
-        find_position = True
+
+    if (position+len(short_list))>len(current_list):
+        return new_list, False
+
+   
+    find_position = True
+    for j in range(len(short_list)):
+        if new_list[position+j] != -1 and new_list[position+j] != short_list[j]:
+            find_position = False
+            break
+    if find_position:
         for j in range(len(short_list)):
-            if new_list[i+j] != -1 and new_list[i+j] != short_list[j]:
-                find_position = False
-                break
-        if find_position:
-            for j in range(len(short_list)):
-                new_list[i+j] = short_list[j]
-            return new_list, i, True
-    return new_list, -1, False
+            new_list[position+j] = short_list[j]
+        return new_list, True
+    return new_list, False
 
 
 def brute_force(long_string_len, short_string_len):
@@ -72,7 +81,7 @@ def brute_force(long_string_len, short_string_len):
             if short_string in long_string:
                 result_dict[short_string] = result_dict[short_string]+1
 
-    print(result_dict)
+    # print(result_dict)
     posibility = sum([v/(2**long_string_len)
                       for v in result_dict.values()])/len(result_dict)
     print(posibility)
