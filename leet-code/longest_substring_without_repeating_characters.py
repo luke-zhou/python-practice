be6f51337dd100c6330ac857c873bf856920cd10
luke <sharlock@gmail.com>
2019-05-20 17:24:25 +1000

    start problem for longest substring without repeating char

---
+++ leet-code/longest_substring_without_repeating_characters.py
@@ -0,0 +1,22 @@
+def lengthOfLongestSubstring(s:str):
+    max = len(set(s))
+            
+    end = 1
+    start = 0
+    longest = 0
+    while end-start<=max and start<len(s) and start+end<=len(s):
+        test_string = s[start:end]
+        print(end)
+        print(start)
+        print(test_string)
+        if len(test_string)>len(set(test_string)):
+            start += 1
+            end += 1
+        else:
+            longest = end-start
+            end += 1
+    
+    return longest    
+
+if __name__=='__main__':
+    print(lengthOfLongestSubstring('pwwkew'))
