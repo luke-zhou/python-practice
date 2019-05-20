9777e7d6e11fe232054e162f601d4562422e9f15
luke <sharlock@gmail.com>
2019-05-20 21:29:53 +1000

    solve longest sub string problem

--- leet-code/longest_substring_without_repeating_characters.py
+++ leet-code/longest_substring_without_repeating_characters.py
@@ -1,33 +1,34 @@
-def lengthOfLongestSubstring(s: str) -> int:
-    max = len(set(s))
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max = len(set(s))
 
-    end = 1
-    start = 0
-    longest = 0
-    while end-start<=max and start<len(s) and end<=len(s):
-        test_string = s[start:end]
-        duplicate_index = findDuplicate(test_string)
-        if duplicate_index !=-1:
-            start += 1 + duplicate_index
-            end += 1 + duplicate_index
-        else:
-            longest = end-start
-            end += 1
+        end = 1
+        start = 0
+        longest = 0
+        while end-start<=max and start<len(s) and end<=len(s):
+            test_string = s[start:end]
+            duplicate_index = self.findDuplicate(test_string)
+            if duplicate_index !=-1:
+                start += 1 + duplicate_index
+                end += 1 + duplicate_index
+            else:
+                longest = end-start
+                end += 1
 
-    return longest     
-    
-def findDuplicate(s: str) -> int:
-    index_dic = {}
-    for i, c in enumerate(s):
-        if c not in index_dic:
-            index_dic[c] =i
-        else:
-            return index_dic[c]
-    
-    return -1 
+        return longest     
+        
+    def findDuplicate(self, s: str) -> int:
+        index_dic = {}
+        for i, c in enumerate(s):
+            if c not in index_dic:
+                index_dic[c] =i
+            else:
+                return index_dic[c]
+        
+        return -1 
 
 if __name__=='__main__':
-    result = lengthOfLongestSubstring('abcadc')
+    result = Solution().lengthOfLongestSubstring('abcadc')
     print(result)
     # result = findDuplicate('pabkww')
     # print(result)
